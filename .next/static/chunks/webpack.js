b54e5ab743c9647069c3d9860444793eaa8bd24d
Harshit Agarwal <harshitagarwal@Harshits-MacBook-Air.local>
2023-06-05 17:41:01 +0530

    remove config.js.example

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "28be445c0f4e076e"; }
+/******/ 		__webpack_require__.h = function() { return "273095f4def5534f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
